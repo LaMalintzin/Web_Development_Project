eb40483b113037430f58cda96871cabe0caa528d
LaMalintzin <34371323+MartinGyllstrom@users.noreply.github.com>
2023-07-19 12:19:30 +0200

    Initial commit

---
+++ website/static/templates/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    # Important to keep secret, but this is just demo project. 
+    app.config['SECRET_KEY'] = 'xxxxx xxxxx xxxxx xxxxx'
+
+    return app
